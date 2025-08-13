663c5798d1110efb790729a20314046af26a82f0
Marco Sussitz <sussitzmarco@gmail.com>
2025-08-13 15:40:22 +0200

    otel changes

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" == "with-tracing" ]; then
   echo "Starting with OpenTelemetry and Jaeger..."
-  docker-compose -f docker-compose.yml -f docker-compose-otel.yml up
+  docker compose -f docker-compose-otel.yaml up
 elif [ "$1" == "no-tracing" ]; then
   echo "Starting without tracing..."
-  docker-compose -f docker-compose.yml up
+  docker compose -f docker-compose.yaml up
 else
   echo "Usage: ./start.sh [no-tracing|with-tracing]"
 fi
