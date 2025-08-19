c477128f7599c8fcdf963b64029f3d84b3fe6f76
Marco Sussitz <sussitzmarco@gmail.com>
2025-08-19 10:09:59 +0200

    update data sql script with different entries

--- dataBaseService/src/main/resources/data.sql
+++ dataBaseService/src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO guestbook_entry (name, message, timestamp) VALUES
-('Alice', 'Welcome to the guestbook!', '2025-08-13 12:00:00');
+('Oak', 'This website has saved my blind dog from a burning house!', '1995-05-23 12:00:00'),
+('Green', 'Thanks to this website multithreading became so much easier!', '2004-09-30 12:00:00'),
+('Llama', 'My back pain has been healed thanks to the cat!', '2014-03-18 00:00:00');
